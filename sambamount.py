83a35edda9e5f6c49558a717c638b60d38b4e403
valueerror <valueerror@gmail.com>
2020-01-19 13:19:53 +0100

    added umount feature

--- sambamount.py
+++ sambamount.py
@@ -28,6 +28,7 @@ class MeinDialog(QtWidgets.QDialog):
         self.ui.verbinden.clicked.connect(self.verbinden)
         self.ui.passwort.textChanged.connect(self.normalize)
         self.ui.speichern.clicked.connect(self.saveConfig)
+        self.ui.trennen.clicked.connect(self.umount)
    
         #init config
         self.sambastore = os.path.join(scriptdir,'SAMBA.DB')
@@ -47,6 +48,41 @@ class MeinDialog(QtWidgets.QDialog):
             self.ui.radioButton3.setChecked(True)
             self.ui.freigabe.setEnabled(True)
       
+        self.checkRoot()
+        self.checkMount()
+            
+            
+        
+      
+      
+    def checkMount(self):
+        mountpoint = self.ui.mountpoint.text()
+        p1 = run(["df","-h"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+
+        if p1:
+            print(p1)
+            if ( mountpoint in  p1.stdout):
+                self.ui.status1.setText("Netzlaufwerk in Ordner <b>%s</b> eingebunden!" %(mountpoint))
+                self.ui.trennen.setEnabled(True)
+                return True
+            else:
+                self.ui.status1.setText("Nicht verbunden")
+                self.ui.status.setText("")
+                self.ui.trennen.setEnabled(False)
+                return False
+
+
+    def umount(self):
+        mountpoint = self.ui.mountpoint.text()
+        mountpoint = os.path.join(USER_HOME_DIR, mountpoint)
+        p1 = run(["umount",f"{mountpoint}"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        print(p1)
+        time.sleep(2)
+        self.checkMount()
+
+
+    
+    def checkRoot(self):
         #check for root permissions 
         if os.geteuid() != 0:
             print ("You need root access in order to mount a network folder")
@@ -54,9 +90,8 @@ class MeinDialog(QtWidgets.QDialog):
             self.ui.close()
             os.system(command)
             os._exit(0)
-      
-
-      
+    
+    
     def saveConfig(self):
         if self.ui.radioButton1.isChecked():
             self.config['default']['sharetype'] = "1"
@@ -65,7 +100,6 @@ class MeinDialog(QtWidgets.QDialog):
         elif self.ui.radioButton3.isChecked():
             self.config['default']['sharetype'] = "3"
      
-     
         self.config['default']['server'] = self.ui.server.text()
         self.config['default']['customshare'] = self.ui.freigabe.text()
         self.config['default']['mountpoint'] = self.ui.mountpoint.text()
@@ -73,7 +107,63 @@ class MeinDialog(QtWidgets.QDialog):
         with open(self.sambastore, 'w') as configfile:
             self.config.write(configfile)
             self.ui.status.setText("Konfiguration gespeichert")
-      
+        
+        self.checkMount()
+            
+            
+            
+    def verbinden(self):
+        self.ui.status.setText("Anmeldedaten holen")
+        server = self.ui.server.text()
+        benutzername = self.ui.benutzer.text()
+        passwort = self.ui.passwort.text()
+        mountpoint = self.ui.mountpoint.text()
+        mountpoint = os.path.join(USER_HOME_DIR, mountpoint)
+        freigabename = self.ui.freigabe.text()
+        result = False
+        
+        if self.checkMount():
+            self.ui.status.setText("Netzlaufwerk bereits eingebunden")
+            return
+        
+        
+        if not os.path.isdir(mountpoint):
+            os.makedirs(mountpoint)
+            os.chown(mountpoint,int(UID),int(UID))
+        
+        if self.checkUsername(benutzername) and self.checkPW(passwort):
+            if self.ui.radioButton1.isChecked():
+                freigabename = benutzername
+            elif self.ui.radioButton2.isChecked():
+                freigabename = "%s$" %(benutzername)
+            elif self.ui.radioButton3.isChecked(): #ohne benutzerkennung
+                freigabename = self.ui.freigabe.text()
+
+            self.ui.status.setText("Verbindung angefordert")
+            command = ["sudo", "mount", "-t", "cifs", "-o", f"user={benutzername}", "-o", f"password={passwort}", "-o", f"uid={UID}", "-o", f"gid={UID}", f"//{server}/{freigabename}", f"{mountpoint}"] 
+            print(command)
+            result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+            
+        else:
+            self.ui.status.setText("Bitte überprüfen sie die Anmeldedaten")
+            
+        if result:
+            print(result.stderr)
+
+            if ( "denied" in  result.stderr):
+                self.ui.status.setText("Fehlerhafte Anmeldedaten")
+            elif ( "busy" in result.stderr):
+                self.ui.status.setText("Netzlaufwerk bereits eingebunden")
+            elif (result.stderr == ""):
+                self.ui.status.setText("Verbindung hergestellt")
+                time.sleep(2)
+                self.checkMount()
+                self.openFilemanager(mountpoint)
+            else:
+                self.ui.status.setText(result.stderr)
+            
+
+
 
     def checkPW(self,pw):
         if not pw.isspace() and not (' ' in pw) == True and pw != "":
@@ -101,81 +191,19 @@ class MeinDialog(QtWidgets.QDialog):
             self.ui.status.setText("Der Benutzername enthält ungültige Zeichen")
             return False
 
-
     def normalize(self):
         palettedefault = self.ui.passwort.palette()
         palettedefault.setColor(QPalette.Active, QPalette.Base, QColor(255, 255, 255))
         self.ui.passwort.setPalette(palettedefault)
 
 
-
-
-    def verbinden(self):
-
-        self.ui.status.setText("Anmeldedaten holen")
-        server = self.ui.server.text()
-        benutzername = self.ui.benutzer.text()
-        passwort = self.ui.passwort.text()
-        mountpoint = self.ui.mountpoint.text()
-        mountpoint = os.path.join(USER_HOME_DIR, mountpoint)
-        freigabename = self.ui.freigabe.text()
-        result = False
-        
-        if not os.path.isdir(mountpoint):
-            os.makedirs(mountpoint)
-            os.chown(mountpoint,int(UID),int(UID))
-        
-   
-        if self.checkUsername(benutzername) and self.checkPW(passwort):
-            
-            if self.ui.radioButton1.isChecked():
-                freigabename = benutzername
-            elif self.ui.radioButton2.isChecked():
-                freigabename = "%s$" %(benutzername)
-            elif self.ui.radioButton3.isChecked(): #ohne benutzerkennung
-                freigabename = self.ui.freigabe.text()
-        
-            #connect
-            self.ui.status.setText("Verbindung angefordert")
-            command = ["sudo", "mount", "-t", "cifs", "-o", f"user={benutzername}", "-o", f"password={passwort}", "-o", f"uid={UID}", "-o", f"gid={UID}", f"//{server}/{freigabename}", f"{mountpoint}"] 
- 
-            print(command)
-            result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-            
-        else:
-            self.ui.status.setText("Bitte überprüfen sie die Anmeldedaten")
-    
-        
-        
-
-        if result:
-            print(result.stderr)
-
-            if ( "error" in  result.stderr):
-                self.ui.status.setText("Fehlerhafte Anmeldedaten")
-            else:
-                self.ui.status.setText("Verbindung hergestellt")
-                time.sleep(2)
-                self.openFilemanager(mountpoint)
-            
-
-
-
-
-
-
     def openFilemanager(self, mountpoint):
         command="sudo -H -u %s dolphin %s > /dev/null 2>&1" %(USER, mountpoint)
         os.system(command)
 
-
-
     def onAbbrechen(self):    # Exit button
         print("GUI closed")
         self.ui.close()
-
-
-
 
 app = QtWidgets.QApplication(sys.argv)
 dialog = MeinDialog()
